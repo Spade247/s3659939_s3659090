c4ba40f81f3eb395f5ec49272fc8905cf67a6555
Yonas Sisay <s3659939@student.rmit.edu.au>
2019-03-29 18:02:46 +1100

    Created a get_config_file method to return a dictionary of the json file

--- Monitor.py
+++ Monitor.py
@@ -10,9 +10,19 @@ config = {
 
 #A method that creates the config.json file with the ranges 
 def create_config_file():
-    with open("config.json","w") as json_file:
+    with open("config.json","w") as json_file:  
         json.dump(config,json_file,indent = 4)
 
+#A method that returns the config.json file into a Dictionary
+def get_config_file():
+    with open("config.json","r") as json_file:
+        config_file = json.load(json_file)
+    return config_file
+
+#Test DATA
+create_config_file()
+d1 = get_config_file()
+print(d1)
 
 
 
