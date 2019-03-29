6ffc90bd370693854e987d016bb956b234073fd2
Yonas Sisay <s3659939@student.rmit.edu.au>
2019-03-29 21:32:08 +1100

    Added success message

--- monitorAndNotify.py
+++ monitorAndNotify.py
@@ -1,5 +1,7 @@
 import Monitor as mon
 import Database as db
+import time 
+from virtual_sense_hat import VirtualSenseHat as vsh
 import Notification as notif
 
 #defaulted to 1 second but should be 60 seconds
@@ -12,11 +14,14 @@ config = mon.get_config_file()
 
 #main function 
 def main():
+    sense = vsh.getSenseHat()
+    #the range is set to every second per minute
     for i in range(0,60):
         db.createDB()
         db.getSenseHatData()
-        db.time.sleep(freq)
+        time.sleep(freq)
     db.displayData()
+    sense.show_message("Recorded Data!",text_colour = [0,255,0])
 
 #execute main program
 main()
