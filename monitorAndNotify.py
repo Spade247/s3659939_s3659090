9df0de2a922367c94b2aa0f6ef35228c7cd27ade
Yonas Sisay <s3659939@student.rmit.edu.au>
2019-03-29 22:38:07 +1100

    changed the range freq condition

--- monitorAndNotify.py
+++ monitorAndNotify.py
@@ -4,7 +4,7 @@ import time
 from virtual_sense_hat import VirtualSenseHat as vsh
 import Notification as notif
 
-#defaulted to 1 second 
+#defaulted to 1 record per minute
 freq = 1
 #Creates the config file with the ranges if it hasnt already been created
 mon.create_config_file()
@@ -15,8 +15,9 @@ config = mon.get_config_file()
 #main function 
 def main():
     sense = vsh.getSenseHat()
-    #the range is set to every second per minute
-    for i in range(freq):
+
+    #the range is set to frequency of the records being created (1 per minute depending on the cronjob)
+    for i in range(0,freq):
         db.createDB()
         db.getSenseHatData()
         time.sleep(freq)
