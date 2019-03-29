7d4668b152e476327870ef5a638ce8e7d564186c
Yonas Sisay <s3659939@student.rmit.edu.au>
2019-03-29 17:22:51 +1100

    Created a method that creates the config.json file with the ranges

--- Monitor.py
+++ Monitor.py
@@ -1,11 +1,19 @@
 import json 
 
+#Created A Dictionary to store the temperature and humidity ranges
 config = {
             "min_temperature":20,
             "max_temperature":30,
-            "min_humdity":50,
-            "max_humidity":60,
+            "min_humidty":50,
+            "max_humidty":60,
             }
-json_string = json.dumps(config)
 
-print(json_string)
+#A method that creates the config.json file with the ranges 
+def create_config_file():
+    with open("config.json","w") as json_file:
+        json.dump(config,json_file,indent = 4)
+
+
+
+
+
